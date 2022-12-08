fbf9ca3f7bc90c0b59aa748c45466fa24b173b9a
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 14:08:15 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
     let p = document.createElement('p')
     // let p = document.querySelector('p')
     p.setAttribute('id', 'para')
-    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
+    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     // p.classList.remove('d-none')
     // p.classList.add('show')
     
