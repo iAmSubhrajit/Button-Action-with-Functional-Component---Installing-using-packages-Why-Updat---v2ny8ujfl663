39ebe787b5618b2fab80c13d64e6f1712627d462
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 13:54:44 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,19 @@ import "./../styles/App.css";
 function App() {
 
   function CreateText() {
-    let p = document.querySelector('p')
+    let p = document.createElement('p')
+    // let p = document.querySelector('p')
     p.setAttribute('id', 'para')
     p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    p.classList.remove('d-none')
-    p.classList.add('show')
+    // p.classList.remove('d-none')
+    // p.classList.add('show')
+    
   }
 
   return (
     <div id="main">
       <button id='click' onClick={CreateText}>Click</button>
-      <p className='d-none'></p>
+      {/* <p className='d-none'></p> */}
     </div>
   );
 }
