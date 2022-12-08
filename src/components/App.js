6d93fa1923d0af392c59fbeb64c476e87e69633c
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 12:14:45 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  function para(){
+      let p = document.createElement('p');
+      p.id = 'para'
+      p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+
+      document.getElementById('main').appendChild(p)
+      
+    }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={para}>Click</button>
+
     </div>
   );
 }
