dc325abc352099879e9bbd768ecf393f89b16fdc
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 13:44:10 +0530

    update p tag

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,9 @@ import "./../styles/App.css";
 function App() {
 
   function CreateText() {
-    // let p = document.createElement('p');
-    // p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
-    // document.getElementById('main').appendChild(p)
-    
-    let p = document.getElementById('para')
+    let p = document.querySelector('p')
     p.setAttribute('id', 'para')
-    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
+    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     p.classList.remove('d-none')
     p.classList.add('show')
   }
