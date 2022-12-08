8fab6f1f446bf80a7d34328736fce2aba2ba8cb6
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 13:20:23 +0530

    added p tag

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import "./../styles/App.css";
 function App() {
 
   function CreateText() {
-    let p = document.createElement('p');
-    p.setAttribute('id', 'para')
-    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
-    document.getElementById('main').appendChild(p)
+    // let p = document.createElement('p');
+    // p.setAttribute('id', 'para')
+    // p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
+    // document.getElementById('main').appendChild(p)
+    
+    let p = document.getElementById('para')
+    p.classList.remove('d-none')
+    p.classList.add('show')
   }
 
   return (
     <div id="main">
       <button id='click' onClick={CreateText}>Click</button>
+      <p id='para' className='d-none'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </div>
   );
 }
