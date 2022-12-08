9102f80954816a19fd6c2fd762ce64d2d8bf280a
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 12:31:14 +0530

    p tag added and text() update

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  function para(){
-      let p = document.createElement('p');
-      p.id = 'para'
-      p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
 
-      document.getElementById('main').appendChild(p)
-      
+  function text(){
+      let p = document.querySelector('p');
+      p.id = 'para'
+      p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"      
     }
+
   return (
     <div id="main">
-      <button id='click' onClick={para}>Click</button>
-
+      <button id='click' onClick={text}>Click</button>
+      <p></p>
     </div>
   );
 }
