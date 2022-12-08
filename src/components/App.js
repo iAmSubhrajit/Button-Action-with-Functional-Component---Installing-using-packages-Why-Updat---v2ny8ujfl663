30dc5f095e301c79cddd77b504155d13b5e31fef
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-08 13:10:50 +0530

    u

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
 
-  function text(){
-      let p = document.createElement('p');
-      p.setAttribute('id','para')
-      p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy" 
-      document.getElementById('main').appendChild(p)     
-    }
+  function CreateText() {
+    let p = document.createElement('p');
+    p.setAttribute('id', 'para')
+    p.innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy "
+    document.getElementById('main').appendChild(p)
+  }
 
   return (
     <div id="main">
-      <button id='click' onClick={text}>Click</button>
+      <button id='click' onClick={CreateText}>Click</button>
     </div>
   );
 }
